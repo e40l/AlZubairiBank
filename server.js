4c08cc51aa33a4f15b8ed3436830d650a201c531
e40l <e40lte@gmail.com>
2025-09-22 16:51:40 +0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-const walletRoutes = require('./routes/walletRoutes');
-app.use('/api/wallets', walletRoutes);
-
-app.listen(3000, ()=> console.log("Bank Alzubair API running on port 3000"));
